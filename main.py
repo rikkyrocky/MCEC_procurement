3a3e64cd51b4fde90aa4a1546b3907dd2f6a4aa3
ricksarkar <adityas0017@gmail.co>
2023-03-04 22:13:35 +1100

    Large update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from logindetails import email, password, batchname
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import os
+import traceback
 import re
 class Invoice:
     pass
@@ -58,6 +59,12 @@ class Ubot:
             button_click("/html/body/div/form/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div[2]/div/div/div[2]/input")
         except Exception as e:
             print(f"timing exception, error {e}")
+            try:
+                button_click(
+                    "/html/body/div/form/div/div/div[2]/div[1]/div/div/div/div/div/div[3]/div/div[2]/div/div[3]/div[2]/div/div/div[2]/input")
+                print("no 2fa required")
+            except:
+                print("fail")
     def open_batch(self): #opening a batch
         def input_fields(xpath_input, str):
             field = self.driver.find_element('xpath', xpath_input)
@@ -126,43 +133,64 @@ class Ubot:
             double_click_coords("/html/body/div[2]/ux-dialog-container[2]/div/div/div/div[2]/div/div/section/div/div[1]/div[2]/section/div/div/div[1]/div/div[5]/div/div/div[8]", 0, 20)
         except Exception as e:
             print(f"failed to start process, error: {e}")
+
         path = "/Users/ricksarkar/PycharmProjects/MCEC_procurement/Invoices"
         Invoices = os.listdir(path)
         print(f"There are {len(Invoices)} Invoices to process")
         for invoice in Invoices:
             invoice_info = {}
-            invoice_info['Invoice_path'] = f"{path}/invoice"
+            invoice_info['Invoice_path'] = f"{path}/{invoice}"
             print(invoice_info['Invoice_path'])
-            invoice_info['invoice_number'] = "823514" #input("Invoice number?:")
-            invoice_info['PO'] = "10359531" #input("PO #?:")
-            invoice_info['date'] = "130223" #input("date? (ddmmyy):")
-            invoice_info['gst'] = "0" #input("gst amount?:")
-            invoice_info['amount'] = "145.71" #input("Invoice amount?:")
-            invoice_info['#lineitems'] = "3" #str(input("# of line items?"))
+            invoice_info['invoice_number'] = "F17777369" #input("Invoice number?:")
+            invoice_info['PO'] = "10359498" #input("PO #?:")
+            invoice_info['date'] = "170223" #input("date? (ddmmyy):")
+            invoice_info['gst'] = "2" #input("gst amount?:")
+            invoice_info['amount'] = "219.64" #input("Invoice amount?:")
+            invoice_info['#lineitems'] = "1" #str(input("# of line items?"))
 
         sleep(2)
         #click add and add from PO item
         try:
             button_click("/html/body/div[2]/ux-dialog-container[3]/div/div/div/div[2]/div/div/section/form/div/div/div[2]/div[2]/div/div/div[1]/div[1]/div/div/div/div[2]/a[2]")
-            sleep(0.15)
-            click_coords("/html/body/div[2]/ux-dialog-container[3]/div/div/div/div[2]/div/div/section/form/div/div/div[2]/div[2]/div/div/div[1]/div[1]/div/div/div/div[2]/a[2]", 0, 90)
+            sleep(0.2)
+            click_coords("/html/body/div[2]/ux-dialog-container[3]/div/div/div/div[2]/div/div/section/form/div/div/div[2]/div[2]/div/div/div[1]/div[1]/div/div/div/div[2]/a[2]", 0, 85)
         except Exception as e:
             print(f"failed to open process window, Error: {e}")
+
         sleep(2)
 
         #input items in first window
         try:
             input_fields("/html/body/div[2]/ux-dialog-container[4]/div/div/div/div[2]/div/div/section/form/div/div/div[1]/div[1]/div[3]/fieldset/div/div/div/div/div[1]/div[2]/div/div/div/div/div[1]/input", invoice_info['invoice_number'])
             date_in(invoice_info['date'], "/html/body/div[2]/ux-dialog-container[4]/div/div/div/div[2]/div/div/section/form/div/div/div[1]/div[1]/div[3]/fieldset/div/div/div/div/div[2]/div[2]/div/div/div/div/div[1]/input")
+        except Exception as e:
+            print("failed to load process window")
+            sleep(0.4)
+            button_click("/html/body/div[2]/ux-dialog-container[3]/div/div/div/div[2]/div/div/section/form/div/div/div[2]/div[2]/div/div/div[1]/div[2]/div[1]/span[1]/span[2]/span[2]")
+            button_click(
+                "/html/body/div[2]/ux-dialog-container[3]/div/div/div/div[2]/div/div/section/form/div/div/div[2]/div[2]/div/div/div[1]/div[1]/div/div/div/div[2]/a[2]")
+            sleep(0.15)
+            click_coords(
+                "/html/body/div[2]/ux-dialog-container[3]/div/div/div/div[2]/div/div/section/form/div/div/div[2]/div[2]/div/div/div[1]/div[1]/div/div/div/div[2]/a[2]",
+                0, 90)
+
+
+        try:
             sleep(0.7)
             input_fields("/html/body/div[2]/ux-dialog-container[4]/div/div/div/div[2]/div/div/section/form/div/div/div[2]/div[2]/div/div/div[2]/section/form/div/nav/div[1]/div/div/div/div[1]/div[1]/fieldset/div/div/div/div/div/div[2]/div/div/div/div/div/input", invoice_info['PO'])
             button_click("/html/body/div[2]/ux-dialog-container[4]/div/div/div/div[2]/div/div/section/form/div/div/div[2]/div[2]/div/div/div[2]/section/form/div/nav/div[2]/div[2]/a[1]")
-            openPOlineitems = self.driver.find_elements_by_xpath("//input[@type='checkbox']")
-
+            openPOlineitems = self.driver.find_elements('xpath', "//input[@type='checkbox']")
+            openPOlineitems1 = self.driver.find_elements('xpath', "//input[@id='checkBoxInput']")
             print(openPOlineitems)
-            print(f"there are {str(openPOlineitems)} open PO line items")
+            '''for checkbox in openPOlineitems:
+                print("Checkbox id:", checkbox.get_attribute("id"))
+                print("Checkbox status:", checkbox.is_selected())'''
+            print(f"there are {len(openPOlineitems)} open PO line items")
+            print(f"there are {len(openPOlineitems1)} open PO line items1")
         except Exception as e:
+            traceback_lines = traceback.format_exc().splitlines()
             print(f"error: {e}")
+            print("".join(traceback_lines[:3]))
 
 
 
@@ -171,7 +199,7 @@ class Ubot:
 
 bot = Ubot()
 bot.openUbock()
-sleep(8)
+sleep(4) #usually 8
 bot.open_batch()
 sleep(1)
 bot.process()
